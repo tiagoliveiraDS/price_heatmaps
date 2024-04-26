782c482f8cd50195d32265a67c9024167117ccef
Tiago Oliveira <tiago.oliveira@devscope.net>
2024-04-26 18:37:16 +0100

    Refactor MongoDB connection configuration in db.js

--- price_heatmaps/src/app/lib/db.js
+++ price_heatmaps/src/app/lib/db.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const connect = async () => {
   try {
-    await mongoose.connect("mongodb+srv://admin:chVnoqDG7sKhmOjl@cluster0.y3fjmoe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect("mongodb+srv://admin:chVnoqDG7sKhmOjl@cluster0.y3fjmoe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {});
     console.log("Connected to MongoDB");
   } catch (error) {
     throw new Error("Error connecting to MongoDB");
