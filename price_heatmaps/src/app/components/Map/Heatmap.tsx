0044111f2a0851d3c33b9057d81d9315b939bf81
Tiago Oliveira <tiago.oliveira@devscope.net>
2024-04-26 21:43:32 +0100

    Refactor Heatmap component to dynamically calculate max intensity value

--- price_heatmaps/src/app/components/Map/Heatmap.tsx
+++ price_heatmaps/src/app/components/Map/Heatmap.tsx
@@ -1,9 +1,14 @@
+'use client';
+
 import { Property } from '@/app/models/Property';
 import { HeatmapLayerFactory } from '@vgrid/react-leaflet-heatmap-layer';
 
 const HeatmapLayer = HeatmapLayerFactory<[number, number, number]>();
 
 export default function Heatmap( {params}: {params: {properties: Property[]}}) {
+    const points: any = params.properties.map((p) => [p.latitude, p.longitude, p.price]);
+
+    const max = Math.max(...points.map((p:any) => p[2]));
 
     return (
         <HeatmapLayer
@@ -11,7 +16,7 @@ export default function Heatmap( {params}: {params: {properties: Property[]}}) {
             longitudeExtractor={(p) => p[1]}
             latitudeExtractor={(p) => p[0]}
             intensityExtractor={(p) => p[2]}
-            max = {1000000}
+            max = {max}
             radius={20}
         />
     );
