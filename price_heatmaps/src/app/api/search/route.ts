d94a354e34209d0c4a908af99237b18b426d4a38
Tiago Oliveira <tiago.oliveira@devscope.net>
2024-04-30 13:14:07 +0100

    Refactor MongoDB connection in search route to use connectDB function and update search query to include district, county, and parish fields

--- price_heatmaps/src/app/api/search/route.ts
+++ price_heatmaps/src/app/api/search/route.ts
@@ -1,19 +1,26 @@
-import { Property } from '@/app/models/Property';
 import { connectDB } from '../../lib/db';
 import PropertySchema from '../../lib/PropertySchema';
 import { NextRequest, NextResponse } from 'next/server';
+import { populate } from '@/app/populate';
 
 export async function GET(req: NextRequest) {
     try {
         await connectDB();
-
+    
         const query = req.nextUrl.searchParams.get('q') || '' as string;
 
         if (typeof query !== 'string') {
             throw new Error('Invalid query');
         }
 
-        const data = await PropertySchema.find();
+        const data = await PropertySchema.find({
+            $or: [
+                { district: { $regex: query, $options: 'i' } },
+                { county: { $regex: query, $options: 'i' } },
+                { parish: { $regex: query, $options: 'i' } },
+            ]
+        });
+
         return NextResponse.json(data, { status: 200 });
     } catch (error: any) {
         return NextResponse.json({ error: error.message }, {status: 500 });
