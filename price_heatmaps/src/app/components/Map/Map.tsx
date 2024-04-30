a03760679a0fd7f168a39d5d0a7dfe085a3c0e49
Tiago Oliveira <tiago.oliveira@devscope.net>
2024-04-30 13:37:07 +0100

    Refactor Map component to dynamically import Heatmap module

--- price_heatmaps/src/app/components/Map/Map.tsx
+++ price_heatmaps/src/app/components/Map/Map.tsx
@@ -4,13 +4,14 @@ import 'leaflet/dist/leaflet.css';
 
 import dynamic from 'next/dynamic'
 
-import Heatmap from './Heatmap'
 import { Property } from '@/app/models/Property';
 import styles from './Map.module.css';
  
 const MapContainer = dynamic(() => import('react-leaflet').then((mod) => mod.MapContainer), {ssr: false})
 
 const TileLayer = dynamic(() => import('react-leaflet').then((mod) => mod.TileLayer), {ssr: false})
+
+const Heatmap = dynamic(() => import('./Heatmap'), {ssr: false})
 
 function get_centroid(properties: Property[]) {
   let lats: number[] = [];
@@ -29,8 +30,6 @@ function get_centroid(properties: Property[]) {
 
   return {mean_lat, mean_long};
 }
-
-
 
 export default function Map({params}: {params: {properties: Property[]}}) {
   let centroid = get_centroid(params.properties);
