cef8deae32884e0d4cdc25622bc32d54570ed343
Tiago Oliveira <tiago.oliveira@devscope.net>
2024-04-30 13:13:20 +0100

    Refactor CSS styles and add loading message in search page component

--- price_heatmaps/src/app/search/page.tsx
+++ price_heatmaps/src/app/search/page.tsx
@@ -6,6 +6,8 @@ import useSWR from "swr";
 import { Property } from "../models/Property";
 import styles from "./page.module.css";
 import SearchBar from "../components/SearchBar/SearchBar";
+import { Suspense } from "react";
+import Loading from "../loading";
 
 const getData = async (url: string) => {
     const res = await fetch(url);
@@ -32,15 +34,29 @@ export default function SearchPage() {
         return <div>Failed to load data</div>;
     }
 
+    if (data.length === 0) {
+        return (
+            <>
+                <div className={styles.div}>
+                    <SearchBar />
+                </div>
+                <h2 className={styles.h2}>There is no data in the region "{searchQuery}"</h2>
+            </>);
+    }
+
     return (
         <>
             <div className={styles.div}>
                 <SearchBar />
             </div>
+        <Suspense fallback={<Loading />}>
+            <h2 className={styles.h2}>Showing all properties in "{searchQuery}"</h2>
 
             <div>
                 <Map params={{ properties: data }} />
             </div>
+
+        </Suspense>
         </>
     );
 }
