76efb39299821afd6b78d4658167765924548ff7
Tiago Oliveira <tiago.oliveira@devscope.net>
2024-04-30 13:14:36 +0100

    Refactor Map component CSS styles and update center and zoom props in MapContainer

--- price_heatmaps/src/app/components/Map/Map.tsx
+++ price_heatmaps/src/app/components/Map/Map.tsx
@@ -12,16 +12,36 @@ const MapContainer = dynamic(() => import('react-leaflet').then((mod) => mod.Map
 
 const TileLayer = dynamic(() => import('react-leaflet').then((mod) => mod.TileLayer), {ssr: false})
 
+function get_centroid(properties: Property[]) {
+  let lats: number[] = [];
+  let longs: number[] = [];
+
+  for (var property of properties) {
+    let latitude = property.latitude;
+    let longitude = property.longitude;
+
+    lats.push(latitude);
+    longs.push(longitude);
+  }
+
+  let mean_lat = lats.reduce((a, b) => a + b, 0) / lats.length;
+  let mean_long = longs.reduce((a, b) => a + b, 0) / longs.length;
+
+  return {mean_lat, mean_long};
+}
+
 
 
 export default function Map({params}: {params: {properties: Property[]}}) {
-  //let position = L.latLng(41.146667, -8.604596);
-
+  let centroid = get_centroid(params.properties);
+  if (!centroid) {
+    centroid = {mean_lat: 41.146667, mean_long: -8.604596};
+  }
   return (
     <>
       <MapContainer
-          center={[41.146667, -8.604596]}
-          zoom={13}
+          center={[centroid.mean_lat, centroid.mean_long]}
+          zoom={11}
           className={styles.map}
       >
         <div><Heatmap params={{properties: params.properties}} /></div>
