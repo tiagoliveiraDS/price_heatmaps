ce62a396a3ce8b296c632f8966647f7603f91391
Tiago Oliveira <tiago.oliveira@devscope.net>
2024-05-28 22:11:25 +0100

    Refactor Map component CSS for improved layout and positioning and added markers functionality

--- price_heatmaps/src/app/components/Map/Map.tsx
+++ price_heatmaps/src/app/components/Map/Map.tsx
@@ -7,7 +7,9 @@ import dynamic from 'next/dynamic'
 import { Property } from '@/app/models/Property';
 import styles from './Map.module.css';
 import { useMapEvents } from 'react-leaflet/hooks'
-import { useState } from 'react';
+import { Key, use, useEffect, useState } from 'react';
+import Image from 'next/image';
+import { Marker, Popup } from 'react-leaflet';
 
 const MapContainer = dynamic(() => import('react-leaflet').then((mod) => mod.MapContainer), { ssr: false })
 
@@ -15,66 +17,148 @@ const TileLayer = dynamic(() => import('react-leaflet').then((mod) => mod.TileLa
 
 const Heatmap = dynamic(() => import('./Heatmap'), { ssr: false })
 
-function get_centroid(properties: Property[]) {
-  let lats: number[] = [];
-  let longs: number[] = [];
+interface DuplicateCoords {
+  latitude: number;
+  longitude: number;
+  count: number;
+}
 
-  for (var property of properties) {
-    let latitude = property.latitude;
-    let longitude = property.longitude;
+interface Centroid {
+  mean_lat: number;
+  mean_long: number;
+}
 
-    lats.push(latitude);
-    longs.push(longitude);
+export default function DataMap({ properties }: { properties: Property[] }) {
+  const [visiblePropertiesAccToZoom, setVisiblePropertiesAccToZoom] = useState<any>(undefined);
+  const [showMarkers, setShowMarkers] = useState(true);
+  const [propertiesWithoutDuplicatesCoords, setPropertiesWithoutDuplicatesCoords] = useState<[number, number, number][] | undefined>(undefined);
+  const [centroid, setCentroid] = useState<Centroid>({ mean_lat: 41.146665, mean_long: -8.604594 });
+  const [duplicates, setDuplicates] = useState<DuplicateCoords[] | undefined>(undefined);
+
+  // load the properties and calculate the duplicates and the centroid at the end of the component mount
+  useEffect(() => {
+    if (properties) {
+      const { uniqueProperties, duplicatedCoords } = removeDuplicatesAndGetCoords(properties);
+      setDuplicates(duplicatedCoords);
+      setPropertiesWithoutDuplicatesCoords(uniqueProperties);
+
+      if (propertiesWithoutDuplicatesCoords) {
+        const centroidRes = getCentroid(propertiesWithoutDuplicatesCoords);
+        setCentroid(centroidRes);
+      }
+    } else {
+      noProperties();
+    }
+  }, [properties]);
+
+  const toggleMarkers = () => {
+    setShowMarkers(!showMarkers);
   }
 
-  let mean_lat = lats.reduce((a, b) => a + b, 0) / lats.length;
-  let mean_long = longs.reduce((a, b) => a + b, 0) / longs.length;
+  function noProperties() {
+    return (<div>No data</div>);
+  }
 
-  return { mean_lat, mean_long };
-}
-
-
-function Adjust({ properties, setFilteredProperties }: { properties: Property[], setFilteredProperties: Function }) {
-  const map = useMapEvents({
-    zoomend: () => {
-      const bounds = map.getBounds();
-      const updatedProperties = properties.filter(property =>
-        bounds.contains([property.latitude, property.longitude])
-      );
-      setFilteredProperties(updatedProperties);
-      console.log(updatedProperties.length);
+  // Média
+  function removeDuplicatesAndGetCoords(properties: Property[]): { uniqueProperties: [number, number, number][], duplicatedCoords: DuplicateCoords[] } {
+    const seen = new Map<string, { count: number, totalPrice: number }>();
+    const uniqueProperties: [number, number, number][] = [];
+    const duplicatedCoords: DuplicateCoords[] = [];
+  
+    if (properties) {
+      for (const property of properties) {
+        const key = `${property.latitude},${property.longitude}`;
+        if (seen.has(key)) {
+          const entry = seen.get(key)!;
+          entry.count += 1;
+          entry.totalPrice += property.price;
+        } else {
+          seen.set(key, { count: 1, totalPrice: property.price });
+        }
+      }
+  
+      seen.forEach((value, key) => {
+        const [latitude, longitude] = key.split(",").map(Number);
+        const avgPrice = value.totalPrice / value.count;
+        uniqueProperties.push([latitude, longitude, avgPrice]);
+        
+        if (value.count > 1) {
+          duplicatedCoords.push({ latitude, longitude, count: value.count });
+        }
+      });
     }
-  });
+  
+    return { uniqueProperties, duplicatedCoords };
+  }
 
-  return null;
-}
+  // get the centroid of the properties
+  function getCentroid(properties: [number, number, number][]) {
+    let lats: number[] = [];
+    let longs: number[] = [];
 
-export default function Map({ params }: { params: { properties: Property[] } }) {
-  const [filteredProperties, setFilteredProperties] = useState<Property[]>(params.properties);
+    for (var property of properties) {
+      let latitude = property[0];
+      let longitude = property[1];
 
-  let centroid = get_centroid(params.properties);
-  if (!centroid) {
-    centroid = { mean_lat: 41.146667, mean_long: -8.604596 };
+      lats.push(latitude);
+      longs.push(longitude);
+    }
+
+    const centroid: Centroid = { mean_lat: 0, mean_long: 0 };
+    centroid.mean_lat = lats.reduce((a, b) => a + b, 0) / lats.length;
+    centroid.mean_long = longs.reduce((a, b) => a + b, 0) / longs.length;
+
+    return centroid;
+  }
+
+  // adjust the properties shown on the map according to the zoom level
+  function Adjust({ properties, setFilteredProperties }: { properties: Property[], setFilteredProperties: Function }) {
+    const map = useMapEvents({
+      zoomend: () => {
+        const bounds = map.getBounds();
+        const updatedProperties = properties.filter(property =>
+          bounds.contains([property.latitude, property.longitude])
+        );
+        setFilteredProperties(updatedProperties);
+      }
+    });
+
+    return null;
   }
 
   return (
     <>
-      <MapContainer
-        center={[centroid.mean_lat, centroid.mean_long]}
-        zoom={11}
-        className={styles.map}
-        minZoom={2}
-        
-      >
-        <Adjust 
-          properties={params.properties}
-          setFilteredProperties={setFilteredProperties} />
-        <Heatmap params={{ properties: params.properties }} />
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      </MapContainer>
+      {!duplicates || !propertiesWithoutDuplicatesCoords ? <div>No properties in this region</div> :
+        <>
+          <MapContainer
+            center={[centroid.mean_lat, centroid.mean_long]}
+            zoom={11}
+            className={styles.map}
+            minZoom={2}
+          >
+            <Adjust
+              properties={properties}
+              setFilteredProperties={setVisiblePropertiesAccToZoom} />
+            <Heatmap
+              properties={propertiesWithoutDuplicatesCoords} />
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {showMarkers && duplicates.map((property: DuplicateCoords, index: number) => (
+              <Marker key={index} position={[property.latitude, property.longitude]}>
+                <Popup>
+                  <div>
+                    <p>There are {property.count} properties at this point</p>
+                  </div>
+                </Popup>
+              </Marker>
+            ))}
+          </MapContainer>
+          <button className={styles.button} onClick={toggleMarkers}>{showMarkers ? 'Hide' : 'Show'} Markers</button>
+          <Image src="/scale3.png" alt="Scale" width={400} height={50} className={styles.image} />
+        </>
+      }
     </>
   );
 }
