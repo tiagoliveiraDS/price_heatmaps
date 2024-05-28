5f4e5c5fdb74b2370c7836120f843e681c68c461
Tiago Oliveira <tiago.oliveira@devscope.net>
2024-05-28 22:10:11 +0100

    Update page module CSS for search bar component

--- price_heatmaps/src/app/page.tsx
+++ price_heatmaps/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1 className={styles.h1}>Price Heatmap</h1>
-      <SearchBar />
+      <div className={styles.searchBar}><SearchBar /></div>
       <Image src="/heatmap.png" alt="Heatmap" fill />
     </main>
   );
