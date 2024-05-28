ccfa1e70f56a10b72debd00c010af48d794b7a24
Tiago Oliveira <tiago.oliveira@devscope.net>
2024-05-28 22:11:01 +0100

    Update Heatmap component to optimize performance and improve readability

--- price_heatmaps/src/app/components/Map/Heatmap.tsx
+++ price_heatmaps/src/app/components/Map/Heatmap.tsx
@@ -2,24 +2,41 @@
 
 import { Property } from '@/app/models/Property';
 import { HeatmapLayerFactory } from '@vgrid/react-leaflet-heatmap-layer';
+import { useEffect, useState } from 'react';
 
 const HeatmapLayer = HeatmapLayerFactory<[number, number, number]>();
 
-export default function Heatmap( {params}: {params: {properties: Property[]}}) {
-    const points: any = params.properties.map((p) => [p.latitude, p.longitude, p.price]);
+export default function Heatmap({ properties }: { properties: [number, number, number][] }) {
+    // const [points, setPoints] = useState<any>([]);
+    // const [maxPrice, setMaxPrice] = useState<number>(0);
 
-    const max = Math.max(...points.map((p:any) => p[2]));
+    // useEffect(() => {
+    //     if (properties) {
+    //         setPoints(properties);
+    //         const maxPriceRes = getMaximumPrice(properties);
+    //         setMaxPrice(maxPriceRes);
+    //     }
+    // }, [properties]);
 
+    const maxPrice = getMaximumPrice(properties);
+    function getMaximumPrice(properties: [number, number, number][]) {
+        return Math.max(...properties.map((p) => p[2]));
+    }
     return (
-        <HeatmapLayer
-            points={params.properties.map((p) => [p.latitude, p.longitude, p.price])}
-            longitudeExtractor={(p) => p[1]}
-            latitudeExtractor={(p) => p[0]}
-            intensityExtractor={(p) => p[2]}
-            max = {max}
-            radius={20}
-            fitBoundsOnLoad={true}
-            fitBoundsOnUpdate={true}
-        />
+        <>
+            <HeatmapLayer
+                points={properties}
+                longitudeExtractor={(p) => p[1]}
+                latitudeExtractor={(p) => p[0]}
+                intensityExtractor={(p) => p[2]}
+                max={maxPrice}
+                radius={20}
+                fitBoundsOnLoad={true}
+                fitBoundsOnUpdate={true}
+                useLocalExtrema={true}
+                opacity={0.9}
+            />
+        </>
+
     );
 }
