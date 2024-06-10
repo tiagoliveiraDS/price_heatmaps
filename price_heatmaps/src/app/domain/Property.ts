65fdfdbbac43ec847c4f2c83423a517bebd265ab
Tiago Oliveira <tiago.oliveira@devscope.net>
2024-06-10 20:43:50 +0100

    Add Property interface for representing property data

---
+++ price_heatmaps/src/app/domain/Property.ts
@@ -0,0 +1,10 @@
+export interface Property {
+    id: string;
+    region: string;
+    price: number;
+    latitude: number;
+    longitude: number;
+    district: string;
+    county: string;
+    parish: string;
+}
