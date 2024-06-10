d3d6dce16bbbf418d293391c4ddbe4b1e1950763
Tiago Oliveira <tiago.oliveira@devscope.net>
2024-06-10 20:42:27 +0100

    Refactor code to use domain folder for Property model and update imports

--- price_heatmaps/src/app/search/page.tsx
+++ price_heatmaps/src/app/search/page.tsx
@@ -3,46 +3,41 @@
 import { useSearchParams } from "next/navigation";
 import DataMap from "../components/Map/Map";
 import useSWR from "swr";
-import { Property } from "../models/Property";
+import { Property } from "../domain/Property";
 import styles from "./page.module.css";
 import SearchBar from "../components/SearchBar/SearchBar";
 import { Suspense, useEffect, useState } from "react";
 import Loading from "../loading";
+import { useDependencies } from "../../dependencies.context";
+import { set } from "mongoose";
 
-const SearchPage = () => {
+export default function SearchPage() {
+    const { propertyService } = useDependencies()
     const search = useSearchParams();
     const searchQuery = search ? search.get("q") : null;
     const encodedQuery = encodeURI(searchQuery || "")
 
-
     const [fetchedData, setFetchedData] = useState<any>();
 
-    const { data, isLoading } = useSWR<Property[]>(`api/search?q=${encodedQuery}`, getData)
-
     useEffect(() => {
-        setFetchedData(data)
-    }, [data]);
-
-    async function getData(url: string) {
-        const res = await fetch(url);
-
-        if (!res.ok) {
-            throw new Error("Failed to fetch data");
-        }
-
-        return res.json();
-    }
+        propertyService.listPropertiesInLocation(encodedQuery)
+            .then((data) => {
+                setFetchedData(data)})
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, [searchQuery, propertyService]);
 
     return (
         <div className={styles.main}>
             <div className={styles.searchBar}>
                 <SearchBar />
             </div>
+            {!fetchedData ? <Loading/> : 
                 <div className={styles.map}>
                     <DataMap properties={fetchedData} />
                 </div>
+            }
         </div>
     );
 }
-
-export default SearchPage;
