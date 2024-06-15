35faf6736b934dda38599ff0de03d743537a4586
Tiago Oliveira <tiago.oliveira@devscope.net>
2024-06-15 12:17:03 +0100

    Refactor Heatmap component to remove commented out code

--- price_heatmaps/src/app/components/Map/Heatmap.tsx
+++ price_heatmaps/src/app/components/Map/Heatmap.tsx
@@ -7,17 +7,6 @@ import { useEffect, useState } from 'react';
 const HeatmapLayer = HeatmapLayerFactory<[number, number, number]>();
 
 export default function Heatmap({ properties }: { properties: [number, number, number][] }) {
-    // const [points, setPoints] = useState<any>([]);
-    // const [maxPrice, setMaxPrice] = useState<number>(0);
-
-    // useEffect(() => {
-    //     if (properties) {
-    //         setPoints(properties);
-    //         const maxPriceRes = getMaximumPrice(properties);
-    //         setMaxPrice(maxPriceRes);
-    //     }
-    // }, [properties]);
-
     const maxPrice = getMaximumPrice(properties);
     function getMaximumPrice(properties: [number, number, number][]) {
         return Math.max(...properties.map((p) => p[2]));
