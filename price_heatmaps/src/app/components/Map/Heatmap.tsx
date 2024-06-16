9d386a64ebf5848c2bfc0c5f846ae32b3b964356
Tiago Oliveira <tiago.oliveira@devscope.net>
2024-06-16 19:59:43 +0100

    Refactor Heatmap component to remove unused code and optimize rendering

--- price_heatmaps/src/app/components/Map/Heatmap.tsx
+++ price_heatmaps/src/app/components/Map/Heatmap.tsx
@@ -1,24 +1,16 @@
 'use client';
 
-import { Property } from '@/app/domain/Property';
 import { HeatmapLayerFactory } from '@vgrid/react-leaflet-heatmap-layer';
-import { useEffect, useState } from 'react';
-
 const HeatmapLayer = HeatmapLayerFactory<[number, number, number]>();
 
 export default function Heatmap({ properties }: { properties: [number, number, number][] }) {
-    const maxPrice = getMaximumPrice(properties);
-    function getMaximumPrice(properties: [number, number, number][]) {
-        return Math.max(...properties.map((p) => p[2]));
-    }
-    return (
+        return (
         <>
             <HeatmapLayer
                 points={properties}
                 longitudeExtractor={(p) => p[1]}
                 latitudeExtractor={(p) => p[0]}
                 intensityExtractor={(p) => p[2]}
-                max={maxPrice}
                 radius={20}
                 fitBoundsOnLoad={true}
                 fitBoundsOnUpdate={true}
